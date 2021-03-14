ca51677fb3f7cc2307daa4d2e97226f356d7956a
shu zOMG chen <shu.chen@freelancedreams.com>
2021-03-14 11:37:18 +0000

    Implement text to speech, add more bot modes

--- palgrave.py
+++ palgrave.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 import random
+import sys
 import time
 
 import pyaudio
@@ -11,22 +12,93 @@ AUDIO_BITRATE = 44100
 AUDIO_BUFFER = 1024
 
 
-class PalgraveImplementation():
+class SpeakOutput():
+	def __init__(self):
+		# requires that you install pyttsx3
+		try:
+			import pyttsx3
+		except ImportError:
+			raise Exception("Please pip install pyttsx3")
+		engine = pyttsx3.init()
+		engine.setProperty("rate", 125) # words per minute
+		engine.setProperty("volume", 1.0)
+		#voices = engine.getProperty("voices")
+		#for i, voice in enumerate(voices):
+		#	print("{}: age: {} gender: {} id: {} languages: {} name: {}".format(
+		#		i,
+		#		voice.age,
+		#		voice.gender,
+		#		voice.id,
+		#		voice.languages,
+		#		voice.name,
+		#	))
+		engine.setProperty("voice", "english-north")
+		self.engine = engine
+
+	def callback_output_text(self, text):
+		self.engine.say(text)
+		self.engine.runAndWait()
+
+
+class PrintOutput():
 	def __init__(self):
 		pass
 
+	def callback_output_text(self, text):
+		print(text)
+
+
+class BaseRobot():
+	def __init__(self, output):
+		self.output = output
+
+	def respond(self, text):
+		self.output.callback_output_text(text)
+
+
+class EchoImplementation(BaseRobot):
+	"""
+	Annoying bot that just repeats what you say.
+	"""
 	def callback_receive_text(self, text):
-		print("I heard: {}".format(text))
+		self.respond(text)
+
+
+class ReverseImplementation(BaseRobot):
+	"""
+	Bot that reverses what it hears:
+	"Hi how are you" -> "you are how hi"
+	"""
+	def callback_receive_text(self, text):
+		words = text.split()
+		reversed_words = " ".join(reversed(words))
+		self.respond(reversed_words)
+		
+
+class BackwardsImplementation(BaseRobot):
+	"""
+	Bot that repeats you but backwards:
+	"Hi how are you" -> "uoy era woh ih"
+	"""
+	def callback_receive_text(self, text):
+		self.respond(text[::-1])
+
+
+class PalgraveImplementation(BaseRobot):
+	"""
+	The best bot
+	"""
+	def callback_receive_text(self, text):
 		if "palgrave" in text:
-			print("Hello!")
+			self.respond("Hello!")
 		if "i" in text and "bored" in text and not "not" in text:
 			randomnumber = random.random()
 			if randomnumber < 0.5:
-				print("Do a jigsaw")
+				self.respond("Do a jigsaw")
 			else:
-				print("Listen to music")
+				self.respond("Listen to music")
 		if "exit" in text or "quit" in text and not "don't" in text and not "do not" in text:
-			print("Goodbye!")
+			self.respond("Goodbye!")
 			time.sleep(2)
 			quit()
 
@@ -49,12 +121,23 @@ def get_audio_stream():
 		frames_per_buffer = AUDIO_BUFFER,
 	)
 	return stream
-	
 
-def main():
+
+def main(bot_mode):
 	recognizer = get_recognizer()
 	stream = get_audio_stream()
-	robot = PalgraveImplementation()
+	#output = PrintOutput()
+	output = SpeakOutput()
+	if bot_mode == "palgrave":
+		robot = PalgraveImplementation(output=output)
+	elif bot_mode == "echo":
+		robot = EchoImplementation(output=output)
+	elif bot_mode == "reverse":
+		robot = ReverseImplementation(output=output)
+	elif bot_mode == "backwards":
+		robot = BackwardsImplementation(output=output)
+	else:
+		raise Exception("Unknown bot mode: {}".format(bot_mode))
 	while 1:
 		data = stream.read(AUDIO_BUFFER)
 		if recognizer.AcceptWaveform(data):
@@ -62,8 +145,13 @@ def main():
 			result = json.loads(raw_result)
 			text = result.get("text")
 			if text:
+				print("DEBUG MIC IN:'{}'".format(text))
 				robot.callback_receive_text(text)
 
 
 if __name__ == "__main__":
-	main()
+	if len(sys.argv) > 1:
+		bot_mode = sys.argv[1]
+	else:
+		bot_mode = "palgrave"
+	main(bot_mode)
