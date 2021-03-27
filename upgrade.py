339080712998fdfd98beca3647f55e2ea84ed0dd
kaiete <kaiete@aragon-press.com>
2021-03-27 10:38:00 +0000

    Fix my error

--- upgrade.py
+++ upgrade.py
@@ -1,6 +1,6 @@
 from subprocess import call
 from time import sleep
-call("git remote add origin https://github.com/kaiete/InstantPalgrave && git pull --set-upstream origin main")
+call("upgrade.sh")
 print("palgrave: update attempt finished")
 sleep(3)
 quit()
---
+++ upgrade.sh
@@ -0,0 +1,2 @@
+
+git pull --set-upstream origin main
