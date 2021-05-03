62c1289d204dd4f11adb277317623b7b749fc3a4
kaiete <kaiete@aragon-press.com>
2021-05-03 18:29:15 +0100

    fix a LOT of very stupid mistakes

--- ppc.py
+++ ppc.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 print("  |  |")
 print(" \\____/\n\n")
 print("Welcome to the Palgrave Package Center!\nDo you want to (I)nstall or (U)ninstall a package? (or (Q)uit PPC) ",end="")
@@ -6,24 +7,25 @@ x = input()
 x = x.lower()
 if x == "i":
     print("OK! What's the name of the package? (user/repo format, should be on github and branch main should exist)")
-    with input() as pkg:
-        print("[",end="")
-        try:
-            import requests
-        except ModuleNotFoundError:
-            print("\nPPC: error! please pip install requests")
-            quit()
-        print("#",end="")
-        ppcdotjson = requests.get("https://raw.githubusercontent.com/{}/main/ppc.json".format(pkg))
-        print("#]")
+    pkg = input()
+    print("[",end="")
+    try:
+        import requests
+    except ModuleNotFoundError:
+        print("\nPPC: error! please pip install requests")
+        quit()
+    print("#",end="")
+    ppcdotjson = requests.get("https://raw.githubusercontent.com/{}/main/ppc.json".format(pkg))
+    print("#]")
     import json
-    ppcdotjson = json.loads(ppcdotjson.text())
+    ppcdotjson = ppcdotjson.text
+    ppcdotjson = json.loads(ppcdotjson)
     print("You're about to install {} by {}. Is that right? (Y/n) ".format(ppcdotjson["name"],ppcdotjson["author"]),end="")
     y = input()
     if y.lower() != "n":
-        commands = requests.get("https://raw.githubusercontent.com/{}/main/commands.json")
-        with open("~/InstantPalgrave/commands.json","w") as commandfile:
-            commandfile.write(commands.text())
+        commands = requests.get("https://raw.githubusercontent.com/{}/main/commands.json".format(pkg))
+        with open("{}/InstantPalgrave/commands.json".format(os.environ.get("HOME")),"w") as commandfile:
+            commandfile.write(commands.text)
         print("All done! Installed successfully.")
     else:
         print("Cancelled, nothing has been installed.")
